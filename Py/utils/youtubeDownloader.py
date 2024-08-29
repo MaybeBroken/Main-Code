7721d8e48b68b18911dbbcfbb4a02a16be436cdb
MaybeBroken <davidsponseller123@gmail.com>
2024-08-29 07:39:48 -0700

    add support for individual songs in youtubeDownloader

--- Py/utils/youtubeDownloader.py
+++ Py/utils/youtubeDownloader.py
@@ -4,12 +4,18 @@ from threading import Thread
 import os
 
 outputPath = "./youtubeDownloader/"
+threadQueue = []
+
 
 def Download(link):
     youtubeObject = YouTube(link)
     youtubeObject = youtubeObject.streams.get_highest_resolution()
     try:
-        name = youtubeObject.default_filename.replace(' ', '_')
+        name = youtubeObject.default_filename.replace(" ", "_")
+        name = youtubeObject.default_filename.replace("(", "")
+        name = youtubeObject.default_filename.replace(")", "")
+        name = youtubeObject.default_filename.replace("[", "")
+        name = youtubeObject.default_filename.replace("]", "")
         youtubeObject.download(outputPath, filename=name)
         print(f"Download of {name} has completed successfully")
     except:
@@ -18,35 +24,60 @@ def Download(link):
 
 def getPlaylist(link):
     x = Playlist(link)
-    print(x.count)
     return x
 
 
 def _Wrapper(link):
     list = getPlaylist(link)
     global outputPath
-    outputPath += list.title.replace(' ', '_') + "/"
+    outputPath += list.title.replace(" ", "_") + "/"
     for uri in list:
-        Thread(target=Download, daemon=True, args=[uri]).start()
+        thread = Thread(target=Download, daemon=True, args=[uri])
+        thread.start()
+        threadQueue.append(thread)
         sleep(0.1)
+    for id in threadQueue:
+        if id.is_alive():
+            id.join()
     print("\n\n\n*********\nFinished\n*********\n")
 
 
-firstchoice = input("\n\nWelcome to the Downloader Utility!\ndownload or convert? (D/C)   ")
+def _Song(link):
+    global outputPath
+    outputPath += "_songs/"
+    Download(link)
+    print("\n\n\n*********\nFinished\n*********\n")
+
+
+firstchoice = input(
+    "\n\nWelcome to the Downloader Utility!\ndownload or convert? (D/C)   "
+)
+
 print("\n")
 if firstchoice == "d" or firstchoice == "D":
-    url = input(f"\nyt Playlist Url:")
-    if url == "" or url == None or len(url) < 20:
-        url = "https://music.youtube.com/playlist?list=PLt-QnSFN9Gjp2sD8DmeY1B0awsd7tmpP7&si=yqYOMDGHaWBLElwP"
-    print("\n")
-    _Wrapper(url)
+    secondChoice = input("\nIs this a song or a playlist? (S/P)   ")
+    if secondChoice == "p" or secondChoice == "P":
+        url = input(f"\nyt Playlist Url:\n-->  ")
+        if url == "" or url == None or len(url) < 20:
+            url = "https://music.youtube.com/playlist?list=PLt-QnSFN9Gjp2sD8DmeY1B0awsd7tmpP7&si=yqYOMDGHaWBLElwP"
+        print("\n")
+        _Wrapper(url)
+    if secondChoice == "s" or secondChoice == "S":
+        url = input(f"\nyt song Url:\n-->  ")
+        if url == "" or url == None or len(url) < 20:
+            url = "https://music.youtube.com/watch?v=aZti2SC6MFY&si=WtZlNftPk1Im5N1q"
+        print("\n")
+        _Song(url)
 if firstchoice == "c" or firstchoice == "C":
     mpQuery = input("\nConvert all to mp3?: (Y/N)  ")
     if mpQuery == "y" or mpQuery == "Y":
         for i in os.listdir(outputPath):
-            if i!='.DS_Store':
+            if i != ".DS_Store":
                 try:
-                    os.mkdir(os.path.join(outputPath, i, "converted"))
+                    try:
+                        os.mkdir(os.path.join(outputPath, i, "converted"))
+                    except:
+                        None
                     for name in os.listdir(os.path.join(outputPath, i)):
                         print(name)
                         dirInput = os.path.abspath(os.path.join(outputPath, i, name))
@@ -55,6 +86,14 @@ if firstchoice == "c" or firstchoice == "C":
                                 outputPath, i, "converted", name.replace(".mp4", ".mp3")
                             )
                         )
-                        os.system(f"Py/utils/ffmpeg -i {dirInput} {dirOutput}")
+                        thread = Thread(
+                            target=os.system,
+                            args=[f"Py/utils/ffmpeg -v 0 -i {dirInput} {dirOutput}"],
+                            daemon=True,
+                        )
+                        thread.start()
+                        threadQueue.append(thread)
                 except:
                     print("file conversion error")
+        while len(threadQueue) > 0:
+            None
