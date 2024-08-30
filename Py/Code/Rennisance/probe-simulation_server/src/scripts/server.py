ee340088d8b62ef48382885c8bebfd5b3875e7fc
davidsponseller <161887842+davidsponseller@users.noreply.github.com>
2024-08-30 12:58:09 -0600

    add renaissance FD program

---
+++ Py/Code/Rennisance/probe-simulation_server/src/scripts/server.py
@@ -0,0 +1,25 @@
+import asyncio
+import src.scripts.decoder as decoder
+import websockets
+import functools
+import time as t
+startTime = t.monotonic()
+def currentTime(dp):
+    return t.monotonic()-startTime
+async def _echo(websocket, var):
+    clientKey = await websocket.recv()
+    newKey = decoder.decode(clientKey)
+    print(f'SERVER -> {currentTime()}: {websocket.remoteAddress} sent data: [\n{newKey}\n]')
+    var=newKey
+    await websocket.send(decoder.encode(var))
+
+
+async def _buildServe(portNum, var):
+    bound_handler = functools.partial(_echo, var=var)
+    async with websockets.serve(bound_handler, "localhost", int(portNum)):
+        print(f'*********\n:SERVER(notice): listening on port {portNum}\n*********')
+        await asyncio.Future()
+
+
+def startServer(portNum, var):
+    asyncio.run(_buildServe(portNum, var))
