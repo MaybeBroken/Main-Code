38c80545b5ddd45eb2671d9e837bb07ffa420880
MaybeBroken <davidsponseller123@gmail.com>
2024-09-04 13:01:24 -0700

    make a calculator thingy for physics util, add test js, edit index html

---
+++ Py/utils/fourthLawCalculator.py
@@ -0,0 +1,6 @@
+mass_first = int(input("\nmass of first object in Kg:  "))
+mass_second = int(input("\nmass of second object in Kg:  "))
+radius = int(input("\nRadius between objects in Meters:  "))
+g = 6.67*(10^-11)
+result = (g*mass_first*mass_second)/(radius^2)
+print(f"\n\nRESULT IN NM^2/KG^2:\n{result}")
