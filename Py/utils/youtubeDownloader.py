1c6567bb47894af8771e33315751905e58efe261
MaybeBroken <davidsponseller123@gmail.com>
2024-09-19 19:14:11 -0700

    update youtubeDownloader

--- Py/utils/youtubeDownloader.py
+++ Py/utils/youtubeDownloader.py
@@ -1,7 +1,15 @@
+import os
+
+os.system("python3 -m pip install pytubefix")
+os.system("python3 -m pip install requests")
+os.system("python3 -m pip install mutagen")
+from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, APIC, error
 from time import sleep
 from pytubefix import YouTube, Playlist
 from threading import Thread
-import os
+import requests
+
 
 outputPath = "./youtubeDownloader/"
 threadQueue = []
@@ -9,11 +17,13 @@ threadQueue = []
 
 def Download(link, format):
     youtubeObject = YouTube(link)
+    thumbUrl = youtubeObject.thumbnail_url
     if format == "mp4":
         None
         dataObject = youtubeObject.streams.get_highest_resolution()
     elif format == "mp3":
-        dataObject = youtubeObject.streams.get_audio_only(format)
+        dataObject = youtubeObject.streams.filter(only_audio=True)
+        dataObject = dataObject.get_audio_only()
     try:
         name = dataObject.default_filename
     except:
@@ -28,8 +38,15 @@ def Download(link, format):
                 return
     for var in [" ", "(", ")", "[", "]"]:
         name = name.replace(var, "_")
+    if format == "mp3":
+        mp3Check = True
+        name = name.replace(".mp4", "")
+    else:
+        mp3Check = False
     try:
-        dataObject.download(outputPath, filename=name if name != None else "name lost")
+        dataObject.download(
+            outputPath, filename=name if name != None else "name lost", mp3=mp3Check
+        )
         print(f"Download of {name} has completed successfully")
     except:
         print(
@@ -37,6 +54,25 @@ def Download(link, format):
         )
         Download(link, "mp4")
         return
+    if format == "mp3":
+        response = requests.get(thumbUrl)
+        with open(outputPath + name + ".jpg", "wb") as file:
+            file.write(response.content)
+
+        os.system(
+            f'ffmpeg -v 1 -i "{outputPath+name+"."+format}" "{outputPath+"_"+name+"."+format}"'
+        )
+
+        audio = MP3(outputPath + "_" + name + "." + format, ID3=ID3)
+        audio.tags.add(
+            APIC(
+                encoding=3,
+                mime="image/jpeg",
+                type=3,
+                desc="Cover",
+                data=open(outputPath + name + ".jpg", "rb").read(),
+            )
+        )
 
 
 def getPlaylist(link):
@@ -62,20 +98,20 @@ def _Wrapper(link, format):
 def _Song(link, format):
     global outputPath
     outputPath += "_songs/"
-    Download(link)
+    Download(link, format)
     print("\n\n\n*********\nFinished\n*********\n")
 
 
 firstchoice = input(
-    "\n\nWelcome to the Downloader Utility!\ndownload or convert? (D/C)   "
+    "\nAnother project by MaybeBroken\nWelcome to the Youtube Downloader Utility!\ndownload or convert? (D/C)   "
 )
 if firstchoice == "d" or firstchoice == "D":
-    format = input("\nwhich format? mp(3/4):   ")
+    format = input("\nwhich format? mp(3/4):  ")
     if format == "3":
         format = "mp3"
     if format == "4":
         format = "mp4"
-    secondChoice = input("\nIs this a song or a playlist? (S/P)   ")
+    secondChoice = input("\nIs this a song or a playlist? (S/P)  ")
     if secondChoice == "p" or secondChoice == "P":
         url = input(f"\nyt Playlist Url:\n-->  ")
         if url == "" or url == None or len(url) < 20:
