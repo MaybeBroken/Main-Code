1ae85efb81a5730408ecbea73716860a9c995b1f
MaybeBroken <davidsponseller123@gmail.com>
2024-10-23 16:04:31 -0700

    update some more stuff in excel parser

--- Py/Code/AHDSC/PlanetSim/main.py
+++ Py/Code/AHDSC/PlanetSim/main.py
@@ -1,4 +1,5 @@
 import openpyxl as ex
+import formulas as fm
 
 File = ex.open("./book.xlsx")
 
@@ -12,25 +13,21 @@ class system:
         self.systemAge = File["System Builder"]["H5"].value
 
         class planets:
-            def __init__(self) -> None:
-                planetsMade = 0
+            def __init__(self):
+                self._list = []
                 for row in range(6, 102):
-                    if File["System Builder"][f"L{row}"].value != "None":
-                        print(File["System Builder"][f"L{row}"].value)
+                    if not File["System Builder"][f"L{row}"].value == None:
 
                         class planet:
                             def __init__(self) -> None:
-                                File["System Builder"][f"H{row}"].value
+                                self.mass = File["System Builder"][f"L{row}"].value
 
-                        self.__setattr__(str(planetsMade), planet)
-                        planetsMade += 1
+                        self._list.append(planet())
 
         self.planets = planets()
-        for id in self.planets.__dict__:
-            print(f"{id} = {self.planets.__dict__[id]}")
 
 
 data = system()
 
-for id in data.__dict__:
-    print(f"{id} = {data.__dict__[id]}")
+for planet in data.planets._list:
+    print(planet.mass)
