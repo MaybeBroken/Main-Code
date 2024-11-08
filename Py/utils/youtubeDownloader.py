6bbd6c892f2d3b9215e3c46f1746e0fe79740242
MaybeBroken <davidsponseller123@gmail.com>
2024-11-07 20:37:16 -0800

    fix youtubedownloader

--- Py/utils/youtubeDownloader.py
+++ Py/utils/youtubeDownloader.py
@@ -1,6 +1,6 @@
 import os
 from time import sleep
-from pytube import YouTube, Playlist, exceptions
+from pytubefix import YouTube, Playlist, exceptions
 from threading import Thread
 import requests
 from cli import Color
@@ -9,149 +9,94 @@ outputPath = "./youtubeDownloader/"
 threadQueue = []
 
 
-def Download(link, format):
-    youtubeObject = YouTube(link)
-    thumbUrl = youtubeObject.thumbnail_url
-    if format == "mp4":
-        None
-        dataObject = youtubeObject.streams.get_highest_resolution()
-    elif format == "mp3":
-        i=0
-        while i<20:
-            try:
-                i+=1
-                dataObject = youtubeObject.streams.filter(only_audio=True)
-                break
-            except exceptions.VideoUnavailable:
-                if i==19:
-                    print('Failed to fetch video')
-        dataObject = dataObject[0]
-    try:
-        name = dataObject.default_filename
-    except:
-        try:
-            name = dataObject.audio_track_name
-        except:
-            try:
-                _stream = youtubeObject.streams.get_highest_resolution()
-                name = _stream.default_filename
-            except:
-                print(f"{Color.RED}failed fetch at link {Color.RESET}{link} {Color.RED}with format {Color.RESET}{format}")
-                return
-    for var in [" ", "(", ")", "[", "]"]:
-        name = name.replace(var, "_")
+def downloadSong(link, format):
+    yt = YouTube(link, on_progress_callback=print)
     if format == "mp3":
-        mp3Check = True
-        name = name.replace(".mp4", "")
+        ys = yt.streams.get_audio_only()
+        ys.download(outputPath, mp3=True)
     else:
-        mp3Check = False
-    try:
-        dataObject.download(
-            outputPath, filename=name if name != None else "name lost", mp3=mp3Check
-        )
-        print(f"Download of {name} has completed successfully")
-    except:
-        print(
-            f"failed download at link {link} with format {format}, resorting to fallback mp4"
-        )
-        Download(link, "mp4")
-        return
-    if format == "mp3":
-        response = requests.get(thumbUrl)
-        with open(outputPath + name + ".jpg", "wb") as file:
-            file.write(response.content)
-
-        # os.system(
-        #     f'ffmpeg -v 1 -i "{outputPath+name+"."+format}" "{outputPath+name+"."+format}"'
-        # )
-
-        # audio = MP3(outputPath +name + "." + format, ID3=ID3)
-        # audio.tags.add(
-        #     APIC(
-        #         encoding=3,
-        #         mime="image/jpeg",
-        #         type=3,
-        #         desc="Cover",
-        #         data=open(outputPath + name + ".jpg", "rb").read(),
-        #     )
-        # )
+        ys = yt.streams.get_highest_resolution()
+        ys.download(outputPath)
 
 
-def getPlaylist(link):
-    x = Playlist(link)
-    return x
-
-
-def _Wrapper(link, format):
-    list = getPlaylist(link)
+def downloadPlaylist(link, format):
+    pl = Playlist(link)
     global outputPath
-    outputPath += list.title.replace(" ", "_") + f"_{format}/"
-    for uri in list:
-        thread = Thread(target=Download, daemon=True, args=[uri, format])
-        thread.start()
-        threadQueue.append(thread)
-        sleep(0.1)
-    for id in threadQueue:
-        if id.is_alive():
-            id.join()
-    print("\n\n\n*********\nFinished\n*********\n")
+    outputPath += pl.title + f" - {format}/"
+    vId = 0
+    for yt in pl.videos:
+        songName = "(name)"
+        if format == "mp3":
+            ys = yt.streams.get_audio_only()
+            songName = ys.default_filename.replace("/", "-")
+            ys.download(
+                outputPath,
+                filename=f"{vId} | {songName}",
+                mp3=True,
+            )
+        else:
+            songName = ys.default_filename.replace("/", "-")
+            ys = yt.streams.get_highest_resolution()
+            ys.download(outputPath, filename=f"{vId} | {songName}")
+
+        print(f"Finished download of {songName}")
+        vId += 1
 
 
-def _Song(link, format):
-    global outputPath
-    outputPath += "_songs/"
-    Download(link, format)
+def _Wrapper(link, list, format):
+    if list:
+        downloadPlaylist(link, format)
+    if not list:
+        downloadSong(link, format)
     print("\n\n\n*********\nFinished\n*********\n")
 
 
 while True:
-    try:
-        firstchoice = input(
-            f"Another project by {Color.GREEN}MaybeBroken{Color.RESET}\nWelcome to the Youtube Downloader Utility!\nDownload or Convert? (D/C)   "
-        )
-        if firstchoice == "d" or firstchoice == "D":
-            format = input("\nWhich format? mp(3/4):  ")
-            if format == "3":
-                format = "mp3"
-            if format == "4":
-                format = "mp4"
-            secondChoice = input("\nIs this a song or a playlist? (S/P)  ")
-            if secondChoice == "p" or secondChoice == "P":
-                url = input(f"\nyt Playlist Url:\n-->  ")
-                if url == "" or url == None or len(url) < 20:
-                    url = "https://music.youtube.com/playlist?list=PLt-QnSFN9Gjp2sD8DmeY1B0awsd7tmpP7&si=yqYOMDGHaWBLElwP"
-                print("\n")
-                _Wrapper(url, format)
-            if secondChoice == "s" or secondChoice == "S":
-                url = input(f"\nyt song Url:\n-->  ")
-                if url == "" or url == None or len(url) < 20:
-                    url = "https://music.youtube.com/watch?v=aZti2SC6MFY&si=WtZlNftPk1Im5N1q"
-                print("\n")
-                _Song(url, format)
-        if firstchoice == "c" or firstchoice == "C":
-            mpQuery = input("\nConvert all to mp3?: (Y/N)  ")
-            if mpQuery == "y" or mpQuery == "Y":
-                for i in os.listdir(outputPath):
-                    if i != ".DS_Store":
-                        # try:
-                            try:
-                                os.mkdir(os.path.join(outputPath, i, "converted"))
-                            except:
-                                None
-                            for name in os.listdir(os.path.join(outputPath, i)):
-                                dirInput = os.path.abspath(
-                                    os.path.join(outputPath, i, name)
-                                )
-                                dirOutput = os.path.abspath(
-                                    os.path.join(
-                                        outputPath,
-                                        i,
-                                        "converted",
-                                        name.replace(".mp3", ".wav"),
-                                    )
-                                )
-                                os.system(f'ffmpeg -i "{dirInput}" "{dirOutput}"')
-                        # except:
-                            # print("file conversion error")
-    except:
-        None
+
+    firstchoice = input(
+        f"Another project by {Color.GREEN}MaybeBroken{Color.RESET}\nWelcome to the Youtube Downloader Utility!\nDownload or Convert? (D/C)   "
+    )
+    if firstchoice == "d" or firstchoice == "D":
+        format = input("\nWhich format? mp(3/4):  ")
+        if format == "3":
+            format = "mp3"
+        if format == "4":
+            format = "mp4"
+        secondChoice = input("\nIs this a song or a playlist? (S/P)  ")
+        if secondChoice == "p" or secondChoice == "P":
+            url = input(f"\nyt Playlist Url:\n-->  ")
+            if url == "" or url == None or len(url) < 20:
+                url = "https://music.youtube.com/playlist?list=PLt-QnSFN9Gjp2sD8DmeY1B0awsd7tmpP7&si=P8J-srN4y03OWyed"
+            print("\n")
+            _Wrapper(url, True, format)
+        if secondChoice == "s" or secondChoice == "S":
+            url = input(f"\nyt song Url:\n-->  ")
+            if url == "" or url == None or len(url) < 20:
+                url = (
+                    "https://music.youtube.com/watch?v=aZti2SC6MFY&si=WtZlNftPk1Im5N1q"
+                )
+            print("\n")
+            _Wrapper(url, False, format)
+    if firstchoice == "c" or firstchoice == "C":
+        mpQuery = input("\nConvert all to mp3?: (Y/N)  ")
+        if mpQuery == "y" or mpQuery == "Y":
+            for i in os.listdir(outputPath):
+                if i != ".DS_Store":
+                    # try:
+                    try:
+                        os.mkdir(os.path.join(outputPath, i, "converted"))
+                    except:
+                        None
+                    for name in os.listdir(os.path.join(outputPath, i)):
+                        dirInput = os.path.abspath(os.path.join(outputPath, i, name))
+                        dirOutput = os.path.abspath(
+                            os.path.join(
+                                outputPath,
+                                i,
+                                "converted",
+                                name.replace(".mp3", ".wav"),
+                            )
+                        )
+                        os.system(f'ffmpeg -i "{dirInput}" "{dirOutput}"')
+                # except:
+                # print("file conversion error")
