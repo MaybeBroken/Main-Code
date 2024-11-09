7d362e8b17df01a2485369ebba828f3da83286b1
MaybeBroken <davidsponseller123@gmail.com>
2024-11-08 18:31:19 -0800

    make youtubeDownloader grab images, change some stuff

--- Py/utils/youtubeDownloader.py
+++ Py/utils/youtubeDownloader.py
@@ -1,19 +1,60 @@
 import os
 from time import sleep
+import music_tag
 from pytubefix import YouTube, Playlist, exceptions
 from threading import Thread
 import requests
-from cli import Color
+
+
+class Color:
+    GREEN = "\033[92m"
+    LIGHT_GREEN = "\033[1;92m"
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[1;34m"
+    MAGENTA = "\033[1;35m"
+    BOLD = "\033[;1m"
+    CYAN = "\033[1;36m"
+    LIGHT_CYAN = "\033[1;96m"
+    LIGHT_GREY = "\033[1;37m"
+    DARK_GREY = "\033[1;90m"
+    BLACK = "\033[1;30m"
+    WHITE = "\033[1;97m"
+    INVERT = "\033[;7m"
+    RESET = "\033[0m"
+
 
 outputPath = "./youtubeDownloader/"
 threadQueue = []
 
 
+def get(url: str, dest_folder: str, dest_name: str):
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
+    filename = dest_name
+    file_path = os.path.join(dest_folder, filename)
+
+    r = requests.get(url, stream=True)
+    if r.ok:
+        with open(file_path, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1024 * 8):
+                if chunk:
+                    f.write(chunk)
+                    f.flush()
+                    os.fsync(f.fileno())
+    else:  # HTTP status code 4XX/5XX
+        print("img download failed: status code {}\n{}".format(r.status_code, r.text))
+
+    return file_path
+
+
 def downloadSong(link, format):
-    yt = YouTube(link, on_progress_callback=print)
+    yt = YouTube(link)
     if format == "mp3":
         ys = yt.streams.get_audio_only()
         ys.download(outputPath, mp3=True)
+
     else:
         ys = yt.streams.get_highest_resolution()
         ys.download(outputPath)
@@ -23,23 +64,43 @@ def downloadPlaylist(link, format):
     pl = Playlist(link)
     global outputPath
     outputPath += pl.title + f" - {format}/"
+    imgPath = outputPath + "img/"
     vId = 0
     for yt in pl.videos:
         songName = "(name)"
+        songPath = ""
         if format == "mp3":
             ys = yt.streams.get_audio_only()
             songName = ys.default_filename.replace("/", "-")
-            ys.download(
-                outputPath,
-                filename=f"{vId} | {songName}",
-                mp3=True,
-            )
+
+            if not os.path.exists():
+
+                ys.download(
+                    outputPath,
+                    filename=f"{vId} | {songName}",
+                    mp3=False,
+                )
+                get(
+                    yt.thumbnail_url,
+                    imgPath,
+                    "".join(f"{vId} | {songName}".split(".")[0] + ".png"),
+                )
+
+                song = music_tag.load_file(songPath)
+
+                with open(
+                    imgPath + "".join(f"{vId} | {songName}".split(".")[0] + ".png"),
+                    "rb",
+                ) as imgFile:
+                    song["artwork"] = imgFile.read()
+
+                song.save()
+            print(f"Finished download of {songName}")
         else:
             songName = ys.default_filename.replace("/", "-")
             ys = yt.streams.get_highest_resolution()
-            ys.download(outputPath, filename=f"{vId} | {songName}")
+            songPath = ys.download(outputPath, filename=f"{vId} | {songName}")
 
-        print(f"Finished download of {songName}")
         vId += 1
 
 
@@ -87,16 +148,24 @@ while True:
                         os.mkdir(os.path.join(outputPath, i, "converted"))
                     except:
                         None
-                    for name in os.listdir(os.path.join(outputPath, i)):
-                        dirInput = os.path.abspath(os.path.join(outputPath, i, name))
-                        dirOutput = os.path.abspath(
-                            os.path.join(
-                                outputPath,
-                                i,
-                                "converted",
-                                name.replace(".mp3", ".wav"),
+                    path = os.listdir(os.path.join(outputPath, i))
+                    path.sort()
+                    for name in path:
+                        if name.endswith(".mp3"):
+                            dirInput = os.path.abspath(
+                                os.path.join(outputPath, i, name)
                             )
-                        )
-                        os.system(f'ffmpeg -i "{dirInput}" "{dirOutput}"')
+                            dirOutput = os.path.abspath(
+                                os.path.join(
+                                    outputPath,
+                                    i,
+                                    "converted",
+                                    name.replace(".m4a.mp3", ".wav"),
+                                )
+                            )
+                            os.system(
+                                f'./ffmpeg -y -v panic -i "{dirInput}" "{dirOutput}"'
+                            )
+                            print(f"Converted {name}")
                 # except:
                 # print("file conversion error")
