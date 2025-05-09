706149258ac415ff8209cb5a95e5dddfdf8b923d
MaybeBroken <davidsponseller123@gmail.com>
2025-05-08 23:17:39 -0600

    Update runner.py to improve Python installation prompts and add runner.json for PyInstaller configuration

--- Python/Projects/mediaPlayer/runner.py
+++ Python/Projects/mediaPlayer/runner.py
@@ -8,14 +8,14 @@ if is_python_installed:
     print("Python is installed, proceeding with runtime.")
 else:
     if sys.platform == "win32":
-        print("Python is not installed, taking you to the microsoft store.")
-        os.startfile("ms-windows-store://pdp/?ProductId=9PJPW5LDXLZ2")
+        print("Python is not installed, taking you to the Microsoft Store.")
+        os.system("start ms-windows-store://pdp/?ProductId=9NCVDN91XZQP")
     elif sys.platform == "linux":
-        print("Python is not installed, taking you to the python website.")
-        os.system("xdg-open https://www.python.org/downloads/source/")
+        print("Python is not installed, taking you to the Python website.")
+        os.system("xdg-open https://www.python.org/downloads/")
     elif sys.platform == "darwin":
-        print("Python is not installed, taking you to the apple store.")
-        os.system("open https://apps.apple.com/us/app/python-3/id1477388418")
+        print("Python is not installed, taking you to the Python website.")
+        os.system("open https://www.python.org/downloads/")
     else:
         print("Python is not installed, please install it manually.")
         sys.exit(1)
