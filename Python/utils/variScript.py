35cfafe7c3f81ed43261a651c9dad98e2d18d09c
MaybeBroken <davidsponseller123@gmail.com>
2025-10-01 18:01:11 -0600

    Refactor variScript.py to support both app and script modes
    - Added user input for mode selection (1 for app, 2 for script).
    - Implemented separate handling for app and script paths.
    - Enhanced error handling and user prompts for clarity.
    - Updated permissions and quarantine attributes for both apps and scripts.

--- Python/utils/variScript.py
+++ Python/utils/variScript.py
@@ -1,10 +1,29 @@
 import os
 
+mode = input("mode (1=app, 2=script): ")
 while True:
-    try:
-        appName = input("\napp directory (ex. /folder/name.app): \n")
-        os.system(f'chmod +x "{appName}/Contents/MacOs/{appName}"')
-        os.system(f'xattr -d com.apple.quarantine "{appName}/Contents/MacOs/{appName}"')
-        print("finished")
-    except:
+    if mode == "1":
+        try:
+            appName = input("\napp directory (ex. /folder/name.app):\n| -> ")
+            os.system(f'chmod +x "{appName}/Contents/MacOs/{appName}"')
+            os.system(
+                f'xattr -d com.apple.quarantine "{appName}/Contents/MacOs/{appName}"'
+            )
+            print("finished")
+        except:
+            print("\nerr\n")
+    elif mode == "2":
+        try:
+            scriptName = input("\nscript path (ex. /folder/name.py): \n | -> ")
+            if (scriptName.startswith('"') and scriptName.endswith('"')) or (
+                scriptName.startswith("'") and scriptName.endswith("'")
+            ):
+                scriptName = scriptName[1:-1]
+            os.system(f'chmod +x "{scriptName}"')
+            os.system(f'xattr -d com.apple.quarantine "{scriptName}"')
+            print("finished")
+        except:
+            print("\nerr\n")
+    else:
         print("\nerr\n")
+    mode = input("mode (1=app, 2=script):")
